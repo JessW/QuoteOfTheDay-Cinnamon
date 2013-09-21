ce6de98d1044ab545fa7dad364b3e85ce9dc0378
JessW <i.heart.knitting@gmail.com>
2013-09-21 13:30:50 -0500

    Working at this point with randomization, need error handling and some TODOs

--- desklet.js
+++ desklet.js
@@ -1,6 +1,7 @@
 const Cinnamon = imports.gi.Cinnamon;
 const Desklet = imports.ui.desklet;
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
 const Settings = imports.ui.settings;
@@ -23,8 +24,8 @@ MyDesklet.prototype = {
             this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], this.instance_id);
 
             this.settings.bindProperty(Settings.BindingDirection.IN,
-                                     "directory",
-                                     "dir",
+                                     "file",
+                                     "file",
                                      this.on_quote_file_changed,
                                      null);
 	} catch (e) {
@@ -37,9 +38,7 @@ MyDesklet.prototype = {
 	this._quoteContainer.add(this._quote);
         this.setContent(this._quoteContainer);
         this.setHeader(_("Quote"));
-	// changing clock to quote below crashes cinnamon, oops
-	// but do I even need this line?
-        //this._quoteSettings = new Gio.Settings({schema: 'org.cinnamon.desklets.clock'});
+	
 	this.on_quote_file_changed();
         this._updateQuote();
     },
@@ -48,49 +47,63 @@ MyDesklet.prototype = {
 	Mainloop.source_remove(this.timeout);
     },
    
-    // I need this function to exist for the setting binding, but I dunno what to put here yet! 
     on_quote_file_changed: function() {
-       let quoteFileContents = Cinnamon.get_file_contents_utf8_sync("/home/jessica/Quotes/literature");
-       this._quoteFileString = quoteFileContents.toString();
-
-       // Count the %'s (i.e. the number of quotations)
-       let quotes = this._quoteFileString;
-       this._numberOfQuotes = 0;
-       let index = 0;
-
-       while(index !== -1) {
-	  index = quotes.indexOf("%", index);
-	  if (index === -1)
-	     break;  // no more %'s
-	  this._numberOfQuotes++;
-	  index++;
-       }
+       this.file = this.file.replace('~', GLib.get_home_dir());
     },
 
     _updateQuote: function(){
        try {
-       let dateFormat = '%A,%e %B';
-       let displayDate = new Date();
-       // fileContents istanceof String is false
-       //let fileContents = Cinnamon.get_file_contents_utf8_sync("/home/jessica/Quotes/literature");
-       //let this._quoteFileString = fileContents.toString();
-       
+       // Since we update infrequently, reread the file in case it has changed.
+       // TODO: error handling here, e.g. in case file doesn't exist?
+       let quoteFileContents = Cinnamon.get_file_contents_utf8_sync(this.file);
+       this._quoteFileString = quoteFileContents.toString();
+
+       // Ensure first char is '%'
+       // TODO: Probably want to ensure the last is also %, for symmetry
+       if (this._quoteFileString.charAt(0) !== '%') {
+	  this._quoteFileString = '%' + this._quoteFileString;
+       }
+
+       // Now count the number of quotes
+       this._countQuotes();
+
+       // Choose a quote randomly
+       let quoteIndex = Math.floor(Math.random() * this._quoteSeparators.length);
+
+       // Parse chosen quote for display
        let firstIndex = this._quoteFileString.indexOf("%");
-       let secondIndex = this._quoteFileString.indexOf("%", firstIndex + 1); // do i need +1?
-       let substring = this._quoteFileString.substring(firstIndex + 1, secondIndex);
-       this._quote.set_text(substring); // this works
+       let secondIndex = this._quoteFileString.indexOf("%", firstIndex + 1);
+      
+       // TODO: This won't work for the last % if we exclude it from the quoteSep array 
+       let substring = this._quoteFileString.substring(
+	     this._quoteSeparators[quoteIndex] + 1, this._quoteSeparators[quoteIndex+1]);
+       this._quote.set_text(substring);
 
-       //this._quote.set_text(fileContents);  // this works
-       //this._quote.set_text(this._quoteFileString); // this works
-
-       //this._quote.set_text(displayDate.toLocaleFormat(dateFormat));
-       //let index = fileContents.indexOf("a");
-       //this._quote.set_text(index);
-       
-       this.timeout = Mainloop.timeout_add_seconds(100, Lang.bind(this, this._updateQuote));
+       // TODO: get timeout from settings
+       this.timeout = Mainloop.timeout_add_seconds(500, Lang.bind(this, this._updateQuote));
        }catch(e){
 	  global.logError(e);
        }
+    },
+    
+    _countQuotes: function(){
+       // TODO: Now we're not just counting, we're making an array .. may need new name...
+       // TODO: The number of quotations is equal to the number of % ...
+       // Count the %'s (i.e. the number of quotations)
+
+       this._quoteSeparators = [];
+       //this._numberOfQuotes = 0;
+       let index = 0;
+       let allQuotes = this._quoteFileString;
+
+       while (index < allQuotes.length) {
+	  index = allQuotes.indexOf("%", index);
+	  if (index === -1)
+	     break;  // no more %'s
+	  this._quoteSeparators.push(index);
+	  index++;
+       }
+       //TODO: Ending % shouldn't count, if there is one -- see lastIndexOf
     }
 }
 
