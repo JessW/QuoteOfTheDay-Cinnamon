5c48059146f46e0d8861e6ef1bb56f5d52211363
JessW <i.heart.knitting@gmail.com>
2013-09-25 13:24:09 -0500

    Removed user defined quoteSeparator and added error handing in case input file does not exist

--- desklet.js
+++ desklet.js
@@ -33,11 +33,6 @@ MyDesklet.prototype = {
                                      "delay",
                                      this.on_timeout_changed,
                                      null);
-            this.settings.bindProperty(Settings.BindingDirection.IN,
-                                     "quoteSeparator",
-                                     "sep",
-                                     this.on_separator_changed,
-                                     null);
 	} catch (e) {
             global.logError(e);
         } 
@@ -48,10 +43,10 @@ MyDesklet.prototype = {
 	this._quoteContainer.add(this._quote);
         this.setContent(this._quoteContainer);
         this.setHeader(_("Quote"));
+	this._sep = "%";
 	
 	this.on_quote_file_changed();
 	this.on_timeout_changed();
-	this.on_separator_changed();
     },
 
     on_desklet_removed: function() {
@@ -69,36 +64,30 @@ MyDesklet.prototype = {
 	  Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._updateQuote));
     },
 
-    on_separator_changed: function() {
-       // Ensure the separator is a single character
-       if (this.sep.length > 1)
-	  this.sep = this.sep.charAt(0);
-       else if (this.sep.length === 0)
-	  this.sep = "%";
-       
-       this._updateQuote();
-    },
-
     _updateQuote: function(){
        // Since we update infrequently, reread the file in case it has changed.
        // TODO: error handling here, e.g. in case file doesn't exist
+       if (!GLib.file_test(this.file, GLib.FileTest.EXISTS))
+            return;
        let quoteFileContents = Cinnamon.get_file_contents_utf8_sync(this.file);
        let allQuotes = quoteFileContents.toString();
 
        // TODO: Limit the size of the string displayed?
+       // TODO: Add on_desklet_clicked to update quote
+       // TODO: Allow user to select font and font size
 
        // Verify that the file is properly formatted with at least one separator
-       if (allQuotes.indexOf(this.sep) === -1) {
+       if (allQuotes.indexOf(this._sep) === -1) {
 	  this._quote.set_text("");
 	  return;
        }
 
        // Ensure first and last chars are 'sep', for symmetry
-       if (allQuotes.charAt(0) !== this.sep) {
-	  allQuotes = this.sep + allQuotes;
+       if (allQuotes.charAt(0) !== this._sep) {
+	  allQuotes = this._sep + allQuotes;
        }
-       if (allQuotes.lastIndexOf(this.sep) !== allQuotes.length - 1) {
-	  allQuotes = allQuotes + this.sep;
+       if (allQuotes.lastIndexOf(this._sep) !== allQuotes.length - 1) {
+	  allQuotes = allQuotes + this._sep;
        }
 
        // Now find the beginning and end of each quotation
@@ -123,7 +112,7 @@ MyDesklet.prototype = {
        let index = 0;
 
        while (index < allQuotes.length) {
-	  index = allQuotes.indexOf(this.sep, index);
+	  index = allQuotes.indexOf(this._sep, index);
 	  if (index === -1)
 	     break;  // no more separators
 	  this._separators.push(index);
