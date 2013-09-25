df9c25fcbb32894c8121805250bb9d6473dc23f8
JessW <i.heart.knitting@gmail.com>
2013-09-24 21:01:57 -0500

    Added delay and quoteSeparator config properties

--- desklet.js
+++ desklet.js
@@ -28,6 +28,16 @@ MyDesklet.prototype = {
                                      "file",
                                      this.on_quote_file_changed,
                                      null);
+            this.settings.bindProperty(Settings.BindingDirection.IN,
+                                     "delay",
+                                     "delay",
+                                     this.on_timeout_changed,
+                                     null);
+            this.settings.bindProperty(Settings.BindingDirection.IN,
+                                     "quoteSeparator",
+                                     "sep",
+                                     this.on_separator_changed,
+                                     null);
 	} catch (e) {
             global.logError(e);
         } 
@@ -40,7 +50,8 @@ MyDesklet.prototype = {
         this.setHeader(_("Quote"));
 	
 	this.on_quote_file_changed();
-        this._updateQuote();
+	this.on_timeout_changed();
+	this.on_separator_changed();
     },
 
     on_desklet_removed: function() {
@@ -49,61 +60,76 @@ MyDesklet.prototype = {
    
     on_quote_file_changed: function() {
        this.file = this.file.replace('~', GLib.get_home_dir());
+       this._updateQuote();
+    },
+    
+    on_timeout_changed: function() {
+       // TODO: *delay by 60
+       this.timeout = 
+	  Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._updateQuote));
+    },
+
+    on_separator_changed: function() {
+       // Ensure the separator is a single character
+       if (this.sep.length > 1)
+	  this.sep = this.sep.charAt(0);
+       else if (this.sep.length === 0)
+	  this.sep = "%";
+       
+       this._updateQuote();
     },
 
     _updateQuote: function(){
-       try {
        // Since we update infrequently, reread the file in case it has changed.
        // TODO: error handling here, e.g. in case file doesn't exist?
        let quoteFileContents = Cinnamon.get_file_contents_utf8_sync(this.file);
-       this._quoteFileString = quoteFileContents.toString();
+       let allQuotes = quoteFileContents.toString();
 
-       // Ensure first char is '%'
-       // TODO: Probably want to ensure the last is also %, for symmetry
-       if (this._quoteFileString.charAt(0) !== '%') {
-	  this._quoteFileString = '%' + this._quoteFileString;
+       // TODO: Limit the size of the string displayed?
+
+       // Verify that the file is properly formatted with at least one separator
+       if (allQuotes.indexOf(this.sep) === -1) {
+	  this._quote.set_text("");
+	  return;
        }
 
-       // Now count the number of quotes
-       this._countQuotes();
+       // Ensure first and last chars are 'sep', for symmetry
+       if (allQuotes.charAt(0) !== this.sep) {
+	  allQuotes = this.sep + allQuotes;
+       }
+       if (allQuotes.lastIndexOf(this.sep) !== allQuotes.length - 1) {
+	  allQuotes = allQuotes + this.sep;
+       }
+       this._quoteFileString = allQuotes;
 
-       // Choose a quote randomly
-       let quoteIndex = Math.floor(Math.random() * this._quoteSeparators.length);
+       // Now find the beginning and end of each quotation
+       this._findSeparators();
+
+       // Choose a quote randomly, subtract 1 so we don't select the ending separator 
+       let index = Math.floor(Math.random() * (this._separators.length - 1));
 
        // Parse chosen quote for display
-       let firstIndex = this._quoteFileString.indexOf("%");
-       let secondIndex = this._quoteFileString.indexOf("%", firstIndex + 1);
-      
-       // TODO: This won't work for the last % if we exclude it from the quoteSep array 
-       let substring = this._quoteFileString.substring(
-	     this._quoteSeparators[quoteIndex] + 1, this._quoteSeparators[quoteIndex+1]);
+       let substring = allQuotes.substring(
+	     this._separators[index] + 1, this._separators[index+1]);
        this._quote.set_text(substring);
-
-       // TODO: get timeout from settings
-       this.timeout = Mainloop.timeout_add_seconds(500, Lang.bind(this, this._updateQuote));
-       }catch(e){
-	  global.logError(e);
-       }
+      
+       // call _updateQuote again after this.delay minutes 
+       this.timeout = 
+	  Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._updateQuote));
     },
     
-    _countQuotes: function(){
-       // TODO: Now we're not just counting, we're making an array .. may need new name...
-       // TODO: The number of quotations is equal to the number of % ...
-       // Count the %'s (i.e. the number of quotations)
-
-       this._quoteSeparators = [];
-       //this._numberOfQuotes = 0;
+    _findSeparators: function(){
+       this._separators = [];
        let index = 0;
        let allQuotes = this._quoteFileString;
 
        while (index < allQuotes.length) {
-	  index = allQuotes.indexOf("%", index);
+	  index = allQuotes.indexOf(this.sep, index);
 	  if (index === -1)
-	     break;  // no more %'s
-	  this._quoteSeparators.push(index);
+	     break;  // no more separators
+	  this._separators.push(index);
 	  index++;
        }
-       //TODO: Ending % shouldn't count, if there is one -- see lastIndexOf
     }
 }
 
