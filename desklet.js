ddfe2d3c81d1a20e3f67060ee9b40cc33fdae204
JessW <i.heart.knitting@gmail.com>
2013-09-26 20:02:42 -0500

    Restructed to be more like photoFrame

--- desklet.js
+++ desklet.js
@@ -19,6 +19,7 @@ MyDesklet.prototype = {
 
 
 	this.metadata = metadata;
+	this.update_id = null;
 
 	 try {
             this.settings = new Settings.DeskletSettings(this, this.metadata["uuid"], this.instance_id);
@@ -26,96 +27,119 @@ MyDesklet.prototype = {
             this.settings.bindProperty(Settings.BindingDirection.IN,
                                      "file",
                                      "file",
-                                     this.on_quote_file_changed,
+                                     this.on_setting_changed,
                                      null);
             this.settings.bindProperty(Settings.BindingDirection.IN,
                                      "delay",
                                      "delay",
-                                     this.on_timeout_changed,
+                                     this.on_setting_changed,
                                      null);
 	} catch (e) {
             global.logError(e);
         } 
 
-        this._quoteContainer = new St.BoxLayout({vertical:true, style_class: 'quote-container'});
-        this._quote = new St.Label();
-
-	this._quoteContainer.add(this._quote);
-        this.setContent(this._quoteContainer);
         this.setHeader(_("Quote"));
-	this._sep = "%";
-	
-	this.on_quote_file_changed();
-	this.on_timeout_changed();
+	this.sep = "%";
+	this.setup_display();
+    },
+
+    on_setting_changed: function() {
+        if (this.update_id > 0)
+            Mainloop.source_remove(this.update_id);
+        this.update_id = null;
+        this.setup_display();
     },
 
     on_desklet_removed: function() {
 	Mainloop.source_remove(this.timeout);
     },
    
-    on_quote_file_changed: function() {
-       this.file = this.file.replace('~', GLib.get_home_dir());
-       this._updateQuote();
+    setup_display: function() {
+        this._quoteContainer = new St.BoxLayout({vertical:true, style_class: 'quote-container'});
+        this._quote = new St.Label();
+
+	this._quoteContainer.add(this._quote);
+        this.setContent(this._quoteContainer);
+
+	this.updateInProgress = false;
+        this.file = this.file.replace('~', GLib.get_home_dir());
+	
+	this._update_loop();
     },
-    
-    on_timeout_changed: function() {
-       // TODO: *delay by 60
-       this.timeout = 
-	  Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._updateQuote));
+
+    _update_loop: function(){
+        this._update();
+        this.update_id = Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._update_loop));
     },
 
-    _updateQuote: function(){
-       // Since we update infrequently, reread the file in case it has changed.
-       // TODO: error handling here, e.g. in case file doesn't exist
-       if (!GLib.file_test(this.file, GLib.FileTest.EXISTS))
-            return;
-       let quoteFileContents = Cinnamon.get_file_contents_utf8_sync(this.file);
-       let allQuotes = quoteFileContents.toString();
-
-       // TODO: Limit the size of the string displayed?
-       // TODO: Add on_desklet_clicked to update quote
-       // TODO: Allow user to select font and font size
-
-       // Verify that the file is properly formatted with at least one separator
-       if (allQuotes.indexOf(this._sep) === -1) {
-	  this._quote.set_text("");
+    _update: function(){
+       if (this.updateInProgress) {
 	  return;
        }
+       this.updateInProgress = true;
 
-       // Ensure first and last chars are 'sep', for symmetry
-       if (allQuotes.charAt(0) !== this._sep) {
-	  allQuotes = this._sep + allQuotes;
-       }
-       if (allQuotes.lastIndexOf(this._sep) !== allQuotes.length - 1) {
-	  allQuotes = allQuotes + this._sep;
-       }
+       try {
+	  // Since we update infrequently, reread the file in case it has changed.
+	  if (!GLib.file_test(this.file, GLib.FileTest.EXISTS))
+	       return;
+	  this._quote.set_text("abc");
+	  let quoteFileContents = Cinnamon.get_file_contents_utf8_sync(this.file);
+	  let allQuotes = quoteFileContents.toString();
 
-       // Now find the beginning and end of each quotation
-       this._findSeparators(allQuotes);
+	  // TODO: Limit the size of the string displayed?
+	  // TODO: Allow user to select font and font size
 
-       // Choose a quote randomly, subtract 1 so we don't select the ending separator 
-       let index = Math.floor(Math.random() * (this._separators.length - 1));
+	  // Verify that the file is properly formatted with at least one separator
+	  if (allQuotes.indexOf(this.sep) === -1) {
+	     this._quote.set_text("");
+	     return;
+	  }
 
-       // Parse chosen quote for display
-       let substring = allQuotes.substring(
-	     this._separators[index] + 1, this._separators[index+1]);
-       this._quote.set_text(substring);
+	  // Ensure first and last chars are 'sep', for symmetry
+	  if (allQuotes.charAt(0) !== this.sep) {
+	     allQuotes = this.sep + allQuotes;
+	  }
+	  if (allQuotes.lastIndexOf(this.sep) !== allQuotes.length - 1) {
+	     allQuotes = allQuotes + this.sep;
+	  }
 
-       // TODO: Do this like in photoframe 
-       // call _updateQuote again after this.delay minutes 
-       this.timeout = 
-	  Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._updateQuote));
+	  // Now find the beginning and end of each quotation
+	  this._findSeparators(allQuotes);
+
+	  // Choose a quote randomly, subtract 1 so we don't select the ending separator 
+	  let index = Math.floor(Math.random() * (this.separators.length - 1));
+
+	  // Parse chosen quote for display
+	  let substring = allQuotes.substring(
+		this.separators[index] + 1, this.separators[index+1]);
+	  this._quote.set_text(substring);
+
+	  // TODO: Do this like in photoframe 
+	  // call _update again after this.delay minutes 
+	  //this.timeout = 
+	    // Mainloop.timeout_add_seconds(this.delay, Lang.bind(this, this._update));
+	} catch (e) {
+            global.logError(e);
+        } finally {
+            this.updateInProgress = false;
+        }   
     },
+
+    // TODO: Test this
+    on_desklet_clicked: function(event){  
+       this._update();
+    },
+
     
     _findSeparators: function(allQuotes){
-       this._separators = [];
+       this.separators = [];
        let index = 0;
 
        while (index < allQuotes.length) {
-	  index = allQuotes.indexOf(this._sep, index);
+	  index = allQuotes.indexOf(this.sep, index);
 	  if (index === -1)
 	     break;  // no more separators
-	  this._separators.push(index);
+	  this.separators.push(index);
 	  index++;
        }
     }
